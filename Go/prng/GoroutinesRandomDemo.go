4ee06fcccc10f9ba5c266202ead10427f85e4384
nicoboss <nico@bosshome.ch>
2021-03-14 17:10:46 +0100

    Golang Beispiel für PRNG in separatem Thread durch Goroutines erstellt

---
+++ Go/prng/GoroutinesRandomDemo.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Basieren auf C++ splitmix PRNG von Arvid Gerstmann.
+func random(seed uint64, result chan uint64) {
+	for {
+		seed += 0x9E3779B97F4A7C15
+		var z uint64 = seed
+		z = (z ^ (z >> 30)) * 0xBF58476D1CE4E5B9
+		z = (z ^ (z >> 27)) * 0x94D049BB133111EB
+		result <- (z ^ (z >> 31)) >> 31
+	}
+}
+
+func main() {
+	cache := 10
+	tasks := 100
+	result := make(chan uint64, cache)
+	go random(1, result)
+	for i := 0; i < tasks; i++ {
+		value := <-result
+		fmt.Println(value)
+	}
+	fmt.Println("Done")
+}
