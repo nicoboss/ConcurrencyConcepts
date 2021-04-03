9c8fa652ed2818080bfb55378f4670e147822f56
nicoboss <nico@bosshome.ch>
2021-04-03 13:36:24 +0200

    Haskell Beispiel für PRNG mit MVar Channels

---
+++ Haskell/prng/actors_MVar.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Control.Monad (forM_)
+import Control.Concurrent (forkIO)
+import Control.Concurrent.MVar
+import Data.Bits
+import Data.Word
+
+-- Basieren auf C++ splitmix PRNG von Arvid Gerstmann.
+random channel seed = do
+    let currentSeed = seed + 0x9E3779B97F4A7C15 :: Word64
+    let z1 = (currentSeed `xor` (currentSeed `shiftR` 30)) * 0xBF58476D1CE4E5B9
+    let z2 = (z1 `xor` (z1 `shiftR` 27)) * 0x94D049BB133111EB
+    let result = (z2 `xor` (z2 `shiftR` 31)) `shiftR` 31
+    putMVar channel result
+    putStrLn "Generated!"
+    random channel currentSeed
+
+main = do
+    channel <- newEmptyMVar
+    forkIO $ random channel 0
+    forM_ [1..100] $ \_ -> do
+        print =<< takeMVar channel
