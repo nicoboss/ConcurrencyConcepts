7691e002a5431ebcce5ddb55d34bafbf29b051cc
nicoboss <nico@bosshome.ch>
2021-04-03 15:42:24 +0200

    Haskell Beispiel für PRNG mit bounded TChan Channels

---
+++ Haskell/prng/actors_bounded_TChans.hs
@@ -0,0 +1,30 @@
+module Main where
+
+import Control.Monad (forM_)
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Concurrent.QSem (newQSem, waitQSem, signalQSem)
+import Control.Concurrent.STM (atomically)
+import Control.Concurrent.STM.TChan (newTChan, readTChan, writeTChan)
+import Data.Bits
+import Data.Word
+
+-- Basieren auf C++ splitmix PRNG von Arvid Gerstmann.
+main = do
+    channel <- atomically $ newTChan
+    guard <- newQSem 10
+    let seed = 0
+    forkIO (forM_ [1..] $ \i -> do
+            waitQSem guard
+            let currentSeed = seed + (0x9E3779B97F4A7C15 * i) :: Word64
+            let z1 = (currentSeed `xor` (currentSeed `shiftR` 30)) * 0xBF58476D1CE4E5B9
+            let z2 = (z1 `xor` (z1 `shiftR` 27)) * 0x94D049BB133111EB
+            let result = (z2 `xor` (z2 `shiftR` 31)) `shiftR` 31
+            atomically $ writeTChan channel result
+            putStrLn "Generated!"
+        )
+    
+    forM_ [1..100] $ \_ -> do
+        threadDelay (1000)
+        signalQSem guard
+        x <- atomically $ readTChan channel
+        putStrLn $ show x
