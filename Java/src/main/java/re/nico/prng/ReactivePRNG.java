4efeca5526ffe103e3f297ff983636426fc36669
nicoboss <nico@bosshome.ch>
2021-04-24 10:37:36 +0200

    Lesbarkeit von ReactivePRNG verbessert

--- Java/src/main/java/re/nico/prng/ReactivePRNG.java
+++ Java/src/main/java/re/nico/prng/ReactivePRNG.java
@@ -5,25 +5,26 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class ReactivePRNG {
 
-public void run() throws InterruptedException {
-	Flowable.fromCallable(() -> {
-		// Basieren auf C++ splitmix PRNG von Arvid Gerstmann.
-		var seed = 1;
-		final long ulong1 = Long.parseUnsignedLong("11400714819323198485");
-		final long ulong2 = Long.parseUnsignedLong("13787848793156543929");
-		final long ulong3 = Long.parseUnsignedLong("10723151780598845931");
-		for (int i=0; i < 100; ++i) {
-			seed += ulong1;
-			long z = seed;
-			z = (z ^ (z >> 30)) * ulong2;
-			z = (z ^ (z >> 27)) * ulong3;
-			System.out.println("Generated!");
-			System.out.println((z ^ (z >> 31)) >> 31);
-		}
-		return "Done!";
-	}).subscribeOn(Schedulers.io()).observeOn(Schedulers.single()).subscribe(System.out::println,
-			Throwable::printStackTrace);
-	System.out.println("Done!!!");
-}
+    public void run() throws InterruptedException {
+    	Flowable.fromCallable(() -> {
+    		// Basieren auf C++ splitmix PRNG von Arvid Gerstmann.
+    		var seed = 1;
+    		final long ulong1 = Long.parseUnsignedLong("11400714819323198485");
+    		final long ulong2 = Long.parseUnsignedLong("13787848793156543929");
+    		final long ulong3 = Long.parseUnsignedLong("10723151780598845931");
+    		for (int i=0; i < 100; ++i) {
+    			seed += ulong1;
+    			long z = seed;
+    			z = (z ^ (z >> 30)) * ulong2;
+    			z = (z ^ (z >> 27)) * ulong3;
+    			System.out.println("Generated!");
+    			System.out.println((z ^ (z >> 31)) >> 31);
+    		}
+    		return "Done!";
+    	}).subscribeOn(Schedulers.io())
+    		.observeOn(Schedulers.single())
+    		.subscribe(System.out::println, Throwable::printStackTrace);
+    	System.out.println("Done!!!");
+    }
 
 }
